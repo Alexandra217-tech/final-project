adeb185a2b551c8215f25e5171796ad2d9f4d67f
Alexandra <a.a.lisenkova127@gmail.com>
2023-02-14 22:11:30 +0300

    Второй вариант выполнения задачи

---
+++ project1.1/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите размер массива");
+int L = Convert.ToInt32(Console.ReadLine()); 
+string[] arrString;
+string[] array;
+arrString = new string[L];
+array = new string[L];
+int len =0;
+int count =0;
+for (int i=0; i<L; i++)
+{
+    string newString = Console.ReadLine(); 
+    arrString[i] = newString;
+    if (newString.Length<=3)
+    {
+        len++;
+    }
+}
+array = new string[len];
+for (int j=0; j<L; j++)
+{
+    if (arrString[j].Length<=3)
+    {
+        array[count]=arrString[j];
+        count++;
+    }
+}
+
+void PrintArray (string [] coll)
+{
+    int count =coll.Length;
+    for (int j=0; j<count; j++)
+    {
+        Console.WriteLine (coll[j]);
+    }
+}
+
+Console.WriteLine ();
+PrintArray(array);
